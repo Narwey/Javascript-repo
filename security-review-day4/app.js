73206dbc9c62294c45d336ccdaa27622bd710d03
Narwey <benmbark.anouar@gmail.com>
2023-09-26 13:45:31 +0100

    3endk atakl ma3ndkch maghataklch

--- security-review-day4/app.js
+++ security-review-day4/app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const csurf = require('csurf');
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -13,10 +14,19 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use(csurf({ cookie: true }));
 
+// Secret token
+const secretKey = 'your-secret-key';
+
 // Validation and Sanitization Middleware
 const validateLoginForm = [
   body('username').notEmpty().trim().escape(),
   body('password').notEmpty().trim().escape(),
+];
+
+// Sample user data (in production, this would be fetched from a database)
+const users = [
+  { id: 1, username: 'admin', password: 'password', role: 'admin' },
+  { id: 2, username: 'user', password: '123456', role: 'user' },
 ];
 
 // Routes
@@ -30,31 +40,52 @@ app.post('/login', validateLoginForm, (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-
-  // Validate and authenticate the user
-  // Implement appropriate validation and secure authentication mechanisms here
-  // For simplicity, you can use a hardcoded username and password for demonstration purposes
-
+  
   const { username, password } = req.body;
 
-  if (username === 'admin' && password === 'password') {
+  // Check if the user exists and the password is correct
+  const user = users.find((u) => u.username === username && u.password === password);
+
+  if (user) {
+    // Generate a JWT token with the user's ID and role
+    const token = jwt.sign({ id: user.id, role: user.role }, secretKey);
     req.session.isAuthenticated = true;
-    res.redirect('/dashboard');
+    res.cookie('jwtToken', token); // Store the token in a cookie
+    return res.redirect('/dashboard');
   } else {
-    res.redirect('/');
+    return res.redirect('/');
   }
 });
 
 app.get('/dashboard', (req, res) => {
-  // Secure the dashboard route to only allow authenticated users
+  // Check if the user is authenticated using sessions
   if (req.session.isAuthenticated) {
-    res.render('dashboard');
+    // Extract the token from the cookie
+    const token = req.cookies.jwtToken;
+    
+    try {
+      // Verify and decode the JWT token
+      const decoded = jwt.verify(token, secretKey);
+      
+      // Access the user's ID and role from the decoded token
+      const userRole = decoded.role;
+
+      // Perform authorization based on the user's role
+      if (userRole === 'admin') {
+        return res.render('dashboard', { user: 'Admin' });
+      } else {
+        return res.render('dashboard', { user: 'User' });
+      }
+    } catch (error) {
+      return res.redirect('/');
+    }
   } else {
-    res.redirect('/');
+    return res.redirect('/');
   }
 });
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
 
